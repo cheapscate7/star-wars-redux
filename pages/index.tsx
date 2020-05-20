65e07c4a9d3db80dd7e6eba472225570681d500e
Matthew Badcock <matthewlisterb@gmail.com>
2020-05-20 15:34:39 +0200

    initialise home page

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import Head from "next/head";
+
+type HomeProps = {
+};
+
+const Home: NextPage<HomeProps> = () => {
+    return (
+        <>
+            <Head>
+                <meta
+                    name="Description"
+                    content=""
+                />
+            </Head>
+        </>
+    );
+};
+
+export default Home;
