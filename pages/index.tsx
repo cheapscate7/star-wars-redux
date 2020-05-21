f333c7f105b6cb66a57944920e9b09f69fac7d3e
Matthew Badcock <matthewlisterb@gmail.com>
2020-05-21 11:09:24 +0200

    added prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,29 @@
 import { NextPage } from 'next';
-import Head from "next/head";
-import Layout from "../components/Layout";
+import Head from 'next/head';
+import Layout from '../components/Layout';
 import { withApollo } from '../lib/apollo';
-import gql from 'graphql-tag'
-import {useQuery} from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
-const ALL_FILMS = gql`query {
-      allFilms {
-        id
-        title
-      }
-    }`;
+const ALL_FILMS = gql`
+    query {
+        allFilms {
+            id
+            title
+        }
+    }
+`;
 
-
-type HomeProps = {
-};
+type HomeProps = {};
 
 const Home: NextPage<HomeProps> = () => {
-
     const { loading, error, data, fetchMore, networkStatus } = useQuery(
-    ALL_FILMS
-  )
+        ALL_FILMS
+    );
     return (
         <>
             <Head>
-                <meta
-                    name="Description"
-                    content=""
-                />
+                <meta name="Description" content="" />
             </Head>
             <Layout>
                 <h1>Hello</h1>
