546b4f27a031133a59f7b3fb3a71945ce0b37da6
Matthew Badcock <matthewlisterb@gmail.com>
2020-05-21 16:19:46 +0200

    list decoration

--- components/list/listItems/FilmItem.tsx
+++ components/list/listItems/FilmItem.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 
 type FilmItemProps = {
     film: IFilm;
 };
 
 const FilmItem: React.FC<FilmItemProps> = ({ film }) => {
-    return <li>{film.title}</li>;
+    return <Item>{film.title}</Item>;
 };
 
 export default FilmItem;
+
+const Item = styled.li`
+    position: relative;
+    padding: 1em 2em;
+    ${({ theme }) => css`
+        border: 1px solid ${theme.colors.foreground};
+    `};
+
+    &::after {
+        content: '';
+        display: block;
+        width: 0;
+        height: 0;
+        border-top: 1em solid transparent;
+        border-right: 1em solid black;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+`;
